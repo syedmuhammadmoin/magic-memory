e8fc405258112b393a7143016efdb10e7eea1539
Syed Moeen <syedmuhammadmoin@gmail.com>
2022-01-18 16:02:57 +0500

    first update

---
+++ src/components/SingleCard.js
@@ -0,0 +1,9 @@
+import './SingleCard.css';
+export default function SingleCard({ card }) {
+  return (
+    <div className='card'>
+      <img className='front' src={card.src} alt='card front' />
+      <img className='back' src='/img/cover.png' alt='card back' />
+    </div>
+  );
+}
